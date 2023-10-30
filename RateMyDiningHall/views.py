87ecaa3f4428004b12309c04e073bef6d8f30e72
Daniel Lee <daniellee1807741@gmail.com>
2023-10-29 21:22:41 -0700

    DB can now scrape the category of each food (webscrape.py only)

--- RateMyDiningHall/views.py
+++ RateMyDiningHall/views.py
@@ -1,32 +1,56 @@
 from django.shortcuts import render
 from bs4 import BeautifulSoup
 import requests
+import mysql.connector
 
+#connect to mysql db
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="!DaNiEl1807",
+    database="menu_db"
+)
+
+mycursor = mydb.cursor()
+sql="INSERT IGNORE INTO food(name,category) VALUES(%s,%s)"
+        
 def home(request):
+    #keep these variables at none so that we dont get unassigned errors
     menu_data = None
     menu_data_list = ""
+    menu_data = dict()
+    page_to_scrape = None
+    
+    #if lothian, get request from lothian menu
     if 'lothian' in request.GET:
-        #get URL from UCR dining hall menu
-        page_to_scrape = requests.get("https://foodpro.ucr.edu/foodpro/shortmenu.asp?sName=University+of+California%2C+Riverside+Dining+Services&locationNum=02&locationName=Lothian+Residential+Restaurant&naFlag=1&WeeksMenus=This+Week%27s+Menus&myaction=read&dtdate=10%2F28%2F2023")
+        #get request from url
+        page_to_scrape = requests.get("https://foodpro.ucr.edu/foodpro/shortmenu.asp?sName=University%20of%20California%2C%20Riverside%20Dining%20Services&locationNum=02&locationName=Lothian%20Residential%20Restaurant&naFlag=1&_gl=1*157j69u*_ga*NTQ4ODY0ODA5LjE2ODQxODUxNDQ.*_ga_S8BZQKWST2*MTY5ODU1ODE0Mi44NC4xLjE2OTg1NTgzMTcuMC4wLjA.*_ga_Z1RGSBHBF7*MTY5ODU1ODE0Mi44NC4xLjE2OTg1NTgzMTcuMC4wLjA.")
         soup = BeautifulSoup(page_to_scrape.text, "html.parser")
-
-        #scrape daily food menu
-        menu_data = dict()
+        
+        #scrape for data and put into db
         for food in soup.findAll("a", attrs={"name":"Recipe_Desc"}):
             menu_data_list += food.text + "\n"
-        menu_data['food'] = menu_data_list
+            menu_data['food'] = menu_data_list
+            val=(food.text, "")
+            mycursor.execute(sql,val)
         pass
     
+    #if glasgow, get request from glasgow menu
     if 'glasgow' in request.GET:
-        #get URL from UCR dining hall menu
-        page_to_scrape = requests.get("https://foodpro.ucr.edu/foodpro/shortmenu.asp?sName=University+of+California%2C+Riverside+Dining+Services&locationNum=03&locationName=Glasgow&naFlag=1&WeeksMenus=This+Week%27s+Menus&myaction=read&dtdate=11%2F10%2F2023")
+        #get request from url
+        page_to_scrape = requests.get("https://foodpro.ucr.edu/foodpro/shortmenu.asp?sName=University%20of%20California%2C%20Riverside%20Dining%20Services&locationNum=03&locationName=Glasgow&naFlag=1&_gl=1*1iyhhso*_ga*NTQ4ODY0ODA5LjE2ODQxODUxNDQ.*_ga_S8BZQKWST2*MTY5ODU1ODE0Mi44NC4xLjE2OTg1NTgxNDUuMC4wLjA.*_ga_Z1RGSBHBF7*MTY5ODU1ODE0Mi44NC4xLjE2OTg1NTgxNDUuMC4wLjA.")
         soup = BeautifulSoup(page_to_scrape.text, "html.parser")
-
-        #scrape daily food menu
-        menu_data = dict()
+        
+        #scrape for data and put into db
         for food in soup.findAll("a", attrs={"name":"Recipe_Desc"}):
-            menu_data_list += food.text 
-        menu_data['food'] = menu_data_list
+            menu_data_list += food.text + "\n"
+            menu_data['food'] = menu_data_list
+            val=(food.text, "")
+            mycursor.execute(sql,val)
         pass
     
+    #render html to django localhost
     return render(request, 'index.html', {'menu':menu_data})
+
+#commit changes to db
+mydb.commit()
--- webscrape.py
+++ webscrape.py
@@ -1,6 +1,11 @@
+#
+# this is a test file for webscraping to mySQL database
+#
 import mysql.connector
 from bs4 import BeautifulSoup
 import requests
+
+#connect to mysql db
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
@@ -12,13 +17,25 @@ mycursor = mydb.cursor()
 sql="INSERT IGNORE INTO food(name,category) VALUES(%s,%s)"
 
 #get URL from UCR dining hall menu
-page_to_scrape = requests.get("https://foodpro.ucr.edu/foodpro/shortmenu.asp?sName=University+of+California%2C+Riverside+Dining+Services&locationNum=03&locationName=Glasgow&naFlag=1&WeeksMenus=This+Week%27s+Menus&myaction=read&dtdate=10%2F20%2F2023")
+page_to_scrape = requests.get("https://foodpro.ucr.edu/foodpro/shortmenu.asp?sName=University%20of%20California%2C%20Riverside%20Dining%20Services&locationNum=03&locationName=Glasgow&naFlag=1&_gl=1*1iyhhso*_ga*NTQ4ODY0ODA5LjE2ODQxODUxNDQ.*_ga_S8BZQKWST2*MTY5ODU1ODE0Mi44NC4xLjE2OTg1NTgxNDUuMC4wLjA.*_ga_Z1RGSBHBF7*MTY5ODU1ODE0Mi44NC4xLjE2OTg1NTgxNDUuMC4wLjA.")
 soup = BeautifulSoup(page_to_scrape.text, "html.parser")
 
-#scrape daily food menu
-for food in soup.findAll("a", attrs={"name":"Recipe_Desc"}):
-    val=(food.text, "")
-    mycursor.execute(sql,val)
+#find all div elements inside the table
+food_table = soup.find('table', attrs={"bordercolorlight":"black"})
+food_rows = food_table.findAll('div')
 
+#iterate through every single div element to check for category/food
+for category in food_rows:
+    i=0
+    #if the div class is a category set it equal to cats
+    if category['class'][i] == 'shortmenucats':
+        cats = category.find('span', attrs={"style":"color: "})
+    #if the div class is a food set it equal to food and add it to database
+    elif category['class'][i] == 'shortmenurecipes':
+        food = category.find('a', attrs={"name":"Recipe_Desc"})
+        val = (food.text, cats.text)
+        mycursor.execute(sql,val)
+    i+=1
+    
+#commit changes
 mydb.commit()
-#menu = soup.findAll("a", attrs={"name":"Recipe_Desc"})
